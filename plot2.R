d63072958f1bc4fc55e891f8eebf8618a7d8c694
maasal <mikko.salonen@helsinki.fi>
2015-03-03 17:50:52 +0100

    changes to plot2.R ver 2

--- plot2.R
+++ plot2.R
@@ -1,16 +1,16 @@
-raw_data <- read.table("household_power_consumption.txt",header = TRUE,na.strings = "?",sep = ";",
+raw_data <- read.table("household_power_consumption.txt",header = TRUE,,na.strings = "?",sep = ";",
                        colClasses = c("character","character",rep("numeric",7)))
 #dates used 2007-02-01 and 2007-02-02
 library(lubridate)
-raw_data[,1] <- dmy(raw_data[,1])
-raw_data[,2] <- hms(raw_data[,2])
-data <- raw_data[raw_data[,1] == startdate | raw_data[,1] == enddate,]
+head(raw_data)
+raw_data$Day <- dmy(raw_data[,1])
+raw_data$Hour <- hms(raw_data[,2])
 
+subdata <- subset(raw_data, Day <= ymd("2007-02-02"))
+subdata <- subset(subdata, Day >= ymd("2007-02-01"))
+subdata$DayTime <- subdata$Day + subdata$Hour
 
-
-
-
-png(file = "plot1.png",width = 480,height = 480)
-hist(data$Global_active_power, main = "Global Active Power", col ="red",
-     xlab = "Global Active Power (kilowatts)")
+png(file="plot2.png",width = 480,height=480)
+plot(subdata$Global_active_power ~ subdata$DayTime,type = "l", ylab = "Global Active Power (kilowatts)",
+     xlab="")
 dev.off()
